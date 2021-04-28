896fc0cde668235b85e848e83e21a1f3f41d47b6
Jonwodi <>
2021-04-28 14:07:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+
+"""
+For in loop
+"""
+# students = ["James", "John", "Steph"]
+# for s in students:
+#   print("Hi", s)
+
+# ==================================
+
+# print(range(10))
+# print(list(range(10)))
+# print(list(range(3, 6)))
+# print(list(range(3, 12, 2)))
+
+# ==================================
+
+# for x in range(6):
+#   print(x + 1)
+#   print(x * 2)
+
+# for x in range(2, 12):
+#   print(x)
+
+# for i in range(1, 6):
+#   for j in range(i):
+#     print("*", end=" ")
+#   print()
+
+
+
+
+"""
+While loop
+"""
+
+# i = 1 
+# while i < 10:
+#   print(i)
+#   i += 1
+
+  # ==================================
+# break statements
+
+# i = 1 
+# while i < 10:
+#   print(i)
+#   if i == 5:
+#     break
+#   i += 1
+
+  # ==================================
+# continue statements
+
+# i = 1 
+# while i < 5:
+#   i += 1
+#   if i == 3:
+#     continue
+#   print(i)
+  
+
+# for i in range(1, 10):
+#   if i == 3:
+#     break
+#   print(i)
